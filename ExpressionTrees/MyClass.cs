41a20166f12ee033384a4acc0eaaed994921b610
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2023-08-11 12:55:17 +0530

    Add project files.

---
+++ ExpressionTrees/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTrees
+{
+    public class MyClass
+    {
+        public int MyProperty { get; set; } = 5;
+        public string MyMethod(int x, string y) => x + y; 
+    }
+}
---
+++ ExpressionTrees/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using ExpressionTrees;
+
+public class Program
+{
+
+    public static void Main(String[] args)
+    {
+        // Delegates are of two types 1) Func ==> returns something 2) Action ==> doesn't return anything
+        // In Func the last parameter is the return type, rest u can add as many input parameters as you want
+
+        Func<int, int, int> sum = (x, y) => x + y;
+        var SumAdditionValue = sum(2, 3);
+        Console.WriteLine("SumAdditionValue: " + SumAdditionValue);
+
+        // Example with Passing a class as a Parameter in the Func, but we need to create an object and pass to the calling function
+
+        Func<MyClass, string> func = c => c.MyMethod(3,"Test String");
+        MyClass myClass = new MyClass();
+        Console.WriteLine("func(myClass): "+func(myClass));
+
+        Func<MyClass, int> func1 = c => c.MyProperty;
+        Console.WriteLine("func1(myClass): " + func1(myClass));
+    }
+
+
+}
+
